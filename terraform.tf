d107c737767fbf391655be7fbbe3b9f88c83542d
Kateryna Firoozi <kateryna.firoozi@skatteetaten.no>
2022-11-15 20:49:01 +0100

    comments out cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "skatt-m86145-terraform-kurs"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
